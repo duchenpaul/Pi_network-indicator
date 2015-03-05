122fd81479255a9076f5ede9d4315388289ac165
duchenpaul <duchenpaul@gmail.com>
2015-03-05 21:53:36 +0800

    Update network-indicator.py

--- network-indicator.py
+++ network-indicator.py
@@ -4,15 +4,18 @@ import time, urllib
 def blink(y,n,t):
   tmp = 0
   while (tmp < t):
-    subprocess.Popen(['echo 1 > /sys/devices/platform/leds-gpio/leds/led0/brightness'], stdout=subprocess.PIPE, shell=True).communicate()[0]
+    #subprocess.Popen(['echo 1 > /sys/devices/platform/leds-gpio/leds/led0/brightness'], stdout=subprocess.PIPE, shell=True).communicate()[0]
+    subprocess.Popen(['echo 1 > /sys/class/leds/led0/brightness'], stdout=subprocess.PIPE, shell=True).communicate()[0]
     # print("11111")
     time.sleep(y)
-    subprocess.Popen(['echo 0 > /sys/devices/platform/leds-gpio/leds/led0/brightness'], stdout=subprocess.PIPE, shell=True).communicate()[0]
+    #subprocess.Popen(['echo 0 > /sys/devices/platform/leds-gpio/leds/led0/brightness'], stdout=subprocess.PIPE, shell=True).communicate()[0]
+    subprocess.Popen(['echo 0 > /sys/class/leds/led0/brightness'], stdout=subprocess.PIPE, shell=True).communicate()[0]
     # print("00000")
     time.sleep(n)
     tmp += y+n
 
-  subprocess.Popen(['echo 0 > /sys/devices/platform/leds-gpio/leds/led0/brightness'], stdout=subprocess.PIPE, shell=True).communicate()[0]
+  #subprocess.Popen(['echo 0 > /sys/devices/platform/leds-gpio/leds/led0/brightness'], stdout=subprocess.PIPE, shell=True).communicate()[0]
+  subprocess.Popen(['echo 0 > /sys/class/leds/led0/brightness'], stdout=subprocess.PIPE, shell=True).communicate()[0]
   # print("------")
 
 def pulse_blink(t):
